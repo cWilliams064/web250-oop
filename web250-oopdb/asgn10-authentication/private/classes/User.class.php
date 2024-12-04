bdb7ba9b6d38ece9fea3892e53420a4640b6d10b
Courtney Williams <courtneyrwilliams@abtech.students.edu>
2024-12-04 18:13:23 -0500

    File Setup.

---
+++ web250-oopdb/asgn10-authentication/private/classes/User.class.php
@@ -0,0 +1,140 @@
+<?php
+
+class User extends DatabaseObject {
+  
+  static protected $table_name = 'users';
+  static protected $db_columns = ['id', 'firstName', 'lastName', 'email', 'username', 'userLevel', 'hashedPassword'];
+
+  public $id;
+  public $firstName;
+  public $lastName;
+  public $email;
+  public $username;
+  public $userLevel;
+  protected $hashedPassword;
+  public $password;
+  public $confirmPassword;
+  protected $passwordRequired = true;
+
+  public const USER_LEVEL_OPTIONS = [
+    'a' => 'Admin',
+    'm' => 'Member'
+  ];
+
+  public function user_level() {
+    if($this->userLevel > 0) {
+      return self::USER_LEVEL_OPTIONS[$this->userLevel];
+    }
+    else {
+      return "Unknown";
+    }
+  }
+
+  public function __construct($args=[]) {
+    $this->id = $args['id'] ?? '';
+    $this->firstName = $args['firstName'] ?? '';
+    $this->lastName = $args['lastName'] ?? '';
+    $this->email = $args['email'] ?? '';
+    $this->username = $args['username'] ?? '';
+    $this->userLevel = $args['userLevel'] ?? 2;
+    $this->password = $args['password'] ?? '';
+    $this->confirmPassword = $args['confirmPassword'] ?? '';
+  }
+
+  public function fullName() {
+    return "{$this->firstName} {$this->lastName}";
+  }
+
+  protected function set_hashed_password() {
+    $this->hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
+  }
+
+  public function verify_password($password) {
+    return password_verify($password, $this->hashedPassword);
+  }
+
+  protected function create() {
+    $this->set_hashed_password();
+    return parent::create();
+  }
+
+  protected function update() {
+    if($this->password != '') {
+      $this->set_hashed_password();
+      // Validate
+    }
+    else {
+      $this->passwordRequired = false;
+      // Skip
+    }
+    return parent::update();
+  }
+
+  protected function validate() {
+    $this->errors = [];
+  
+    if(is_blank($this->firstName)) {
+      $this->errors[] = "First name cannot be blank.";
+    } elseif (!has_length($this->firstName, array('min' => 2, 'max' => 255))) {
+      $this->errors[] = "First name must be between 2 and 255 characters.";
+    }
+  
+    if(is_blank($this->lastName)) {
+      $this->errors[] = "Last name cannot be blank.";
+    } elseif (!has_length($this->lastName, array('min' => 2, 'max' => 255))) {
+      $this->errors[] = "Last name must be between 2 and 255 characters.";
+    }
+  
+    if(is_blank($this->email)) {
+      $this->errors[] = "Email cannot be blank.";
+    } elseif (!has_length($this->email, array('max' => 255))) {
+      $this->errors[] = "Last name must be less than 255 characters.";
+    } elseif (!has_valid_email_format($this->email)) {
+      $this->errors[] = "Email must be a valid format.";
+    }
+  
+    if(is_blank($this->username)) {
+      $this->errors[] = "Username cannot be blank.";
+    } elseif (!has_length($this->username, array('min' => 8, 'max' => 255))) {
+      $this->errors[] = "Username must be between 8 and 255 characters.";
+    } elseif (!has_unique_username($this->username, $this->id ?? 0)) {
+      $this->errors[] = "Username not allowed. Try another.";
+    }
+  
+    if($this->passwordRequired) {
+      if(is_blank($this->password)) {
+        $this->errors[] = "Password cannot be blank.";
+      } elseif (!has_length($this->password, array('min' => 12))) {
+        $this->errors[] = "Password must contain 12 or more characters";
+      } elseif (!preg_match('/[A-Z]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 uppercase letter";
+      } elseif (!preg_match('/[a-z]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 lowercase letter";
+      } elseif (!preg_match('/[0-9]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 number";
+      } elseif (!preg_match('/[^A-Za-z0-9\s]/', $this->password)) {
+        $this->errors[] = "Password must contain at least 1 symbol";
+      }
+    }
+  
+    if(is_blank($this->confirmPassword)) {
+      $this->errors[] = "Confirm password cannot be blank.";
+    } elseif ($this->password !== $this->confirmPassword) {
+      $this->errors[] = "Password and confirm password must match.";
+    }
+  
+    return $this->errors;
+  }  
+
+  static public function find_by_username($username) {
+    $sql = "SELECT * FROM " . static::$table_name;
+    $sql .= " WHERE username='" . self::$database->escape_string($username) . "'";
+    $obj_array = static::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    }
+    else {
+      return false;
+    }
+  } 
+}
---
+++ web250-oopdb/asgn10-authentication/public/users/form-fields.php
@@ -0,0 +1,50 @@
+<?php
+// prevents this code from being loaded directly in the browser
+// or without first setting the necessary object
+if(!isset($user)) {
+  redirect_to(url_for('../public/users/index.php'));
+}
+?>
+
+<dl>
+  <dt>First name</dt>
+  <dd><input type="text" name="user[firstName]" value="<?php echo h($user->firstName); ?>" /></dd>
+</dl>
+
+<dl>
+  <dt>Last name</dt>
+  <dd><input type="text" name="user[lastName]" value="<?php echo h($user->lastName); ?>" /></dd>
+</dl>
+
+<dl>
+  <dt>Email</dt>
+  <dd><input type="text" name="user[email]" value="<?php echo h($user->email); ?>" /></dd>
+</dl>
+
+<dl>
+  <dt>Username</dt>
+  <dd><input type="text" name="user[username]" value="<?php echo h($user->username); ?>" /></dd>
+</dl>
+
+<dl>
+  <dt>User Level:</dt>
+  <dd>
+    <select name="user[userLevel]">
+      <?php foreach(User::USER_LEVEL_OPTIONS as $id => $userLevel) { ?>
+        <option value="<?php echo $id; ?>" <?php if ($user->userLevel == $id) { echo 'selected'; } ?>>
+          <?php echo $userLevel; ?>
+        </option>
+      <?php } ?>
+    </select>
+  </dd>
+</dl>
+
+<dl>
+  <dt>Password</dt>
+  <dd><input type="password" name="user[password]" value="" /></dd>
+</dl>
+
+<dl>
+  <dt>Confirm Password</dt>
+  <dd><input type="password" name="user[confirmPassword]" value="" /></dd>
+</dl>
